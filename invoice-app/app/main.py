fae79979a9d206cfba8376bbb2ba29fd1207c5e2
James Ockhuis <ockhuisjames@gmail.com>
2023-03-11 23:55:18 +0200

    Render pdf

--- invoice-app/app/main.py
+++ invoice-app/app/main.py
@@ -2,7 +2,9 @@ import io
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import StreamingResponse
 from fastapi.templating import Jinja2Templates
-import pdfkit
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.lib.units import inch
+from reportlab.pdfgen import canvas
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -14,7 +16,16 @@ def home(request: Request):
 @app.post("/invoice/")
 async def generate_invoice(request: Request, client_name: str = Form(...), amount: float = Form(...)):
     invoice_html = templates.TemplateResponse("invoice.html", {"request": request, "client_name": client_name, "amount": amount})
-    invoice_pdf = pdfkit.from_string(invoice_html.body.decode('utf-8'), False)
-    response = StreamingResponse(io.BytesIO(invoice_pdf), media_type='application/pdf')
+    invoice_pdf = canvas.Canvas("invoice.pdf", pagesize=landscape(letter))
+
+    # add text to the PDF document
+    invoice_pdf.setFont("Helvetica-Bold", 24)
+    invoice_pdf.drawString(2 * inch, 10 * inch, "Invoice")
+
+    # save the PDF document
+    invoice_pdf.save()
+    with open("invoice.pdf", "rb") as pdf_file:
+        pdf_data = pdf_file.read()
+    response = StreamingResponse(io.BytesIO(pdf_data), media_type='application/pdf')
     response.headers['Content-Disposition'] = 'attachment; filename="invoice.pdf"'
     return response
--- invoice-app/scripts/run-docker.sh
+++ invoice-app/scripts/run-docker.sh
@@ -1,4 +1,4 @@
 
 docker build -t invoice:dev .
 
-docker run --rm --name -d invoice -p 80:80 invoice:dev
+docker run --rm -d --name invoice -p 80:80 invoice:dev
